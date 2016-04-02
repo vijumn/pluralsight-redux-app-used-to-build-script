4e2eabfbacbb572d6717f3fabff9231684fe09ac
Cory House <housecor@gmail.com>
2016-04-02 09:19:26 -0500

    Updated message and added empty test file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; //Webpack can import CSS files too!
-import './styles/styles.css';
+import './styles/styles.css'; //Webpack can import CSS files too!
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
-    <h1>Welcome to React and Redux in ES6 on Pluralsight!</h1>,
+    <div>
+        <h1>Welcome to React and Redux in ES6!</h1>
+        <p>If you can see this, you're ready to <a href="https://www.pluralsight.com/authors/cory-house">follow along with the course on Pluralsight</a>.</p>
+    </div>,
   document.getElementById('app')
 );
