6277c800743571af0c33b5b22e53c9d109525076
Cory House <housecor@gmail.com>
2016-04-03 07:53:09 -0500

    Module 1 complete.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
-    <div>
-        <h1>Welcome to React and Redux in ES6!</h1>
-        <p>If you can see this, you're ready to <a href="https://www.pluralsight.com/authors/cory-house">follow along with the course on Pluralsight</a>.</p>
-    </div>,
-  document.getElementById('app')
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
 );
