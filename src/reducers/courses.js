b8f8179de87d3648cd8256ffbac56cbb6a640ad3
Cory House <housecor@gmail.com>
2016-04-07 06:56:42 -0500

    Moved constants

--- src/reducers/courses.js
+++ src/reducers/courses.js
@@ -1,11 +1,12 @@
-import * as types from '../constants/actionTypes';
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 // IMPORTANT: Note that with Redux, state should NEVER be changed.
 // State is considered immutable. Instead,
 // create a copy of the state passed and set new values on the copy.
 // Note that I'm using Object.assign to create a copy of current state
 // and update values on the copy.
-export default function courses(state = [], action) {
+export default function courses(state = initialState.courses, action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state,
