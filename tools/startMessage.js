d6b8bf54c19c6fa3447d175d8cfdeeb7581b5573
Cory House <housecor@gmail.com>
2016-04-07 06:56:21 -0500

    Updated startMessage.

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -2,8 +2,5 @@ import colors from 'colors';
 
 /*eslint-disable no-console */
 
-console.log('Starting app in development mode....'.green);
-console.log('Running tests....'.green);
-console.log('Linting...'.green);
-console.log('Opening default browser at http://localhost:3000...'.green);
+console.log('Starting app in dev mode, running tests, linting, and opening default browser at http://localhost:3000...'.green);
 
