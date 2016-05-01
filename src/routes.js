2e841094d5117a34d151a934a95adbbcdc045b13
Cory House <housecor@gmail.com>
2016-05-01 13:43:25 -0500

    Switched to singular folder name.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import CoursesPage from './components/courses/CoursesPage';
+import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 import AboutPage from './components/about/AboutPage';
 
