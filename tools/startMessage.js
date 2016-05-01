aaa9f5852ffc9f3ccffcacc891e333997de232ee
Cory House <housecor@gmail.com>
2016-05-01 10:57:21 -0500

    Simplified start message

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -2,5 +2,4 @@ import colors from 'colors';
 
 /*eslint-disable no-console */
 
-console.log('Starting app in dev mode, running tests, linting, and opening default browser at http://localhost:3000...'.green);
-
+console.log('Starting app in dev mode...'.green);
