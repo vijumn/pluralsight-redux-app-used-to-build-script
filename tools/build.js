6112bb6345877a0a81f6efbca08fa54465aa7d35
Cory House <housecor@gmail.com>
2017-06-06 06:20:52 -0500

    Update colors calls to use clearer API.

--- tools/build.js
+++ tools/build.js
@@ -7,29 +7,29 @@ import colors from 'colors';
 
 process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn't apply.
 
-console.log('Generating minified bundle for production via Webpack. This will take a moment...'.blue);
+console.log(colors.blue('Generating minified bundle for production via Webpack. This will take a moment...'));
 
 webpack(webpackConfig).run((err, stats) => {
   if (err) { // so a fatal error occurred. Stop here.
-    console.log(err.bold.red);
+    console.log(colors.red(err));
     return 1;
   }
 
   const jsonStats = stats.toJson();
 
   if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(error.red));
+    return jsonStats.errors.map(error => console.log(colors.red(error)));
   }
 
   if (jsonStats.hasWarnings) {
-    console.log('Webpack generated the following warnings: '.bold.yellow);
-    jsonStats.warnings.map(warning => console.log(warning.yellow));
+    console.log(colors.yellow.bold('Webpack generated the following warnings: '));
+    jsonStats.warnings.map(warning => console.log(colors.yellow(warning)));
   }
 
   console.log(`Webpack stats: ${stats}`);
 
   // if we got this far, the build succeeded.
-  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green);
+  console.log(colors.green('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'));
 
   return 0;
 });
