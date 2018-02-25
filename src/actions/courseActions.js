f13e19750c382d917b2da2a9f6adf487688fa208
Cory House <Cory.House@coxautoinc.com>
2018-02-25 15:06:29 -0600

    Remove thunks

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,41 +1,19 @@
-import CourseApi from '../api/mockCourseApi';
-import * as types from './actionTypes';
-import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+import CourseApi from "../api/mockCourseApi";
+import * as types from "./actionTypes";
+import { beginAjaxCall, ajaxCallError } from "./ajaxStatusActions";
 
-export function loadCoursesSuccess(courses) {
-  return {type: types.LOAD_COURSES_SUCCESS, courses};
+export function loadCourses() {
+  return { type: types.LOAD_COURSES };
 }
 
 export function createCourseSuccess(course) {
-  return {type: types.CREATE_COURSE_SUCCESS, course};
+  return { type: types.CREATE_COURSE_SUCCESS, course };
 }
 
 export function updateCourseSuccess(course) {
-  return {type: types.UPDATE_COURSE_SUCCESS, course};
-}
-
-// Functions below handle asynchronous calls.
-// Each returns a function that accepts a dispatch.
-// These are used by redux-thunk to support asynchronous interactions.
-export function loadCourses() {
-  return function (dispatch) {
-    dispatch(beginAjaxCall());
-    return CourseApi.getAllCourses().then(courses => {
-      dispatch(loadCoursesSuccess(courses));
-    }).catch(error => {
-      throw(error);
-    });
-  };
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function saveCourse(course) {
-  return function (dispatch, getState) {
-    dispatch(beginAjaxCall());
-    return CourseApi.saveCourse(course).then(course => {
-      course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
-    }).catch(error => {
-      dispatch(ajaxCallError(error));
-      throw(error);
-    });
-  };
+  return { type: types.SAVE_COURSE, course };
 }
