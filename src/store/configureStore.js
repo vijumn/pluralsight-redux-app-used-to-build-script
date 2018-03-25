bb9b5e0ccce72c3c15e0b5c19253a66ec19ddb53
Cory House <Cory.House@coxautoinc.com>
2018-03-25 08:28:36 -0500

    Streamline saga config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,18 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from '../sagas';
-
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware();
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import createSagaMiddleware from "redux-saga";
 
 export default function configureStore(initialState) {
-  return createStore(
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
     rootReducer,
     initialState,
     applyMiddleware(sagaMiddleware, reduxImmutableStateInvariant())
   );
-}
 
-// then run the saga
-sagaMiddleware.run(rootSaga);
+  store.runSaga = sagaMiddleware.run;
+  store.close = () => store.dispatch(END);
+  return store;
+}
