893ece071197accfe04c37c32a62cecb7e548ed1
Cory House <Cory.House@coxautoinc.com>
2018-03-25 08:53:23 -0500

    Add missing action creator

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,6 +1,10 @@
 import AuthorApi from "../api/mockAuthorApi";
 import * as types from "./actionTypes";
 
-export function loadAuthors(authors) {
+export function loadAuthors() {
+  return { type: types.LOAD_AUTHORS };
+}
+
+export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
 }
