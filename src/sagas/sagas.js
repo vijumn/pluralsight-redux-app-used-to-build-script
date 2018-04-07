817f6915a02855046c4528f7d87c2ebdd21cbb19
Cory House <housecor@gmail.com>
2018-04-07 09:46:19 -0500

    fix call to pass arg as sep param, wrap watch in while true and separate take and call.

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-constant-condition */
 /* eslint-disable no-console */
-import { takeLatest, fork, put, call, all, select, take } from "redux-saga/effects";
+import { takeLatest, fork, put, call, all, select, take, takeEvery } from "redux-saga/effects";
 import * as authorActions from "../actions/authorActions";
 import * as courseActions from "../actions/courseActions";
 import * as ajaxActions from "../actions/ajaxStatusActions";
-import * as actionTypes from '../actions/actionTypes';
+import {SAVE_COURSE, LOAD_COURSES} from '../actions/actionTypes';
 import CourseApi from "../api/mockCourseApi";
 import AuthorApi from "../api/mockAuthorApi";
 
@@ -30,23 +30,30 @@ export function* loadCourses() {
 
 export function* saveCourse(course) {
   // yield will suspend the saga until it comples.
-  const savedCourse = yield call(CourseApi.saveCourse(course));
+  const savedCourse = yield call(CourseApi.saveCourse, course);
   // Put is an effect. Effects are just JS objects that contain instructions that are handled by middleware.
   // Sagas are paused while the middlware is processing a yielded effect.
   const action = course.id
-    ? courseActions.createCourseSuccess(savedCourse)
-    : courseActions.updateCourseSuccess(savedCourse);
+    ? courseActions.updateCourseSuccess(savedCourse)
+    : courseActions.createCourseSuccess(savedCourse);
   yield put(action);
 }
 
 // Spawn a new loadCourse task on each LOAD_COURSE
 export function* watchLoadCourses() {
-  yield take(actionTypes.LOAD_COURSES, loadCourses);
+  /*
+    takeEvery will fork a new `loadCourses` task on each LOAD_COURSES action
+    So, concurrent LOAD_COURSES actions are allowed
+  */
+  yield takeEvery(LOAD_COURSES, loadCourses);
 }
 
 // Spawn a new saveCourse task on each SAVE_COURSE
 export function* watchSaveCourse() {
-  yield take(actionTypes.SAVE_COURSE, saveCourse);
+  while(true) {
+    const action = yield take(SAVE_COURSE);
+    yield call(saveCourse, action.course);
+  }
 }
 
 function* watchAndLog() {
