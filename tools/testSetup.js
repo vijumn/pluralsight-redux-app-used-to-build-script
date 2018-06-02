4e654a18370b1a43ab4d043ace92030cea6a075b
Cory House <housecor@gmail.com>
2018-06-02 15:03:30 -0500

    Update jsdom config

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -31,12 +31,12 @@ require.extensions['.jpg'] = function () {return null;};
 
 // Configure JSDOM and set global variables
 // to simulate a browser environment for tests.
-var jsdom = require('jsdom').jsdom;
+var { JSDOM } = require('jsdom');
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
-global.window = document.defaultView;
+const { window } = new JSDOM(`...`);
+const { document } = (new JSDOM(`...`)).window;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
