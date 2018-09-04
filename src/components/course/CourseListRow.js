468adbbba46f6721da3c6aec2522ed3436d17eb7
Cory House <housecor@gmail.com>
2018-09-03 19:04:09 -0500

    Remove blank page open

--- src/components/course/CourseListRow.js
+++ src/components/course/CourseListRow.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const CourseListRow = ({course}) => {
   return (
     <tr>
-      <td><a href={course.watchHref} target="_blank">Watch</a></td>
+      <td><a href={course.watchHref}>Watch</a></td>
       <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
       <td>{course.authorId}</td>
       <td>{course.category}</td>
