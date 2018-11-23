6fbeccd321a26b8003256b4b0e022f2c2da3ba84
Cory House <housecor@gmail.com>
2018-11-23 11:34:54 -0600

    Prettier, remove noErrorsPlugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,46 +1,61 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
 
 export default {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  mode: "development",
+  devtool: "cheap-module-source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client',
-    './src/index'
+    //"eventsource-polyfill", // necessary for hot reloading with IE
+    "webpack-hot-middleware/client?reload=true",
+    "./src/index"
   ],
-  target: 'web',
+  target: "web",
   output: {
-    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: "/",
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: './src'
+    contentBase: "./src"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new HtmlWebpackPlugin({ template: 'src/index.html'})
+    new HtmlWebpackPlugin({ template: "src/index.html" })
   ],
   module: {
     rules: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel-loader'},
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, "src"),
+        loader: "babel-loader"
+      },
       {
         test: /(\.css)$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
-            options: {sourcemap: true}
+            loader: "css-loader",
+            options: { sourcemap: true }
           }
         ]
       },
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
-      {test: /\.(woff|woff2)$/, options: {prefix: 'font/', limit: 5000}, loader: 'url-loader'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, options: {limit: 10000, mimetype: 'application/octet-stream'}, loader: 'url-loader'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, options: {limit: 10000, mimetype: 'image/svg+xml' }, loader: 'url-loader'}
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      {
+        test: /\.(woff|woff2)$/,
+        options: { prefix: "font/", limit: 5000 },
+        loader: "url-loader"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        options: { limit: 10000, mimetype: "application/octet-stream" },
+        loader: "url-loader"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        options: { limit: 10000, mimetype: "image/svg+xml" },
+        loader: "url-loader"
+      }
     ]
   }
 };
