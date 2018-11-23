8cb2c77b731e3c578905547228036effa5fddf6d
Cory House <housecor@gmail.com>
2018-11-23 06:59:44 -0600

    Add comma

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -9,7 +9,7 @@ class AboutPage extends React.Component {
       <div>
         <h1>About</h1>
         <p>
-          This application uses React, Redux, React Router and a variety of
+          This application uses React, Redux, React Router, and a variety of
           other helpful libraries.
         </p>
       </div>
