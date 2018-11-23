08d03d33749de64383b61c113ccc0d1b58221c4d
Cory House <housecor@gmail.com>
2018-11-23 12:51:36 -0600

    Reducer organized by ID. WIP. Just a start

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -7,10 +7,12 @@ import initialState from "./initialState";
 // Note that I'm using Object spread to create a copy of current state
 // and update values on the copy.
 export default function courses(state = initialState.courses, action) {
+  const courses = {};
   switch (action.type) {
     case types.LOAD_COURSES_SUCCESS:
-      return action.courses;
-
+      // return action.courses; // Can first do this for simplicity. Then can show selector approach.
+      action.courses.forEach(course => (courses[course.id] = { ...course }));
+      return courses;
     case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
 
@@ -25,8 +27,24 @@ export default function courses(state = initialState.courses, action) {
 }
 
 // Selectors
-export function getCourseById(courses, id) {
-  const course = courses.find(course => course.id == id);
-  if (course) return course;
-  return null;
+// Since selectors and reducers work with the same data shape,
+// it's useful to compose them together in the same file.
+export function getAllCourses(courses) {
+  return Object.keys(courses).map(courseId => courses[courseId]);
+}
+
+// Used when courses are a plain array
+// export function getCourseById(courses, id) {
+//   const course = courses.find(course => course.id == id);
+//   if (course) return course;
+//   return null;
+// }
+
+// Used when reducer is structured by key
+export function getCourseById(courses, courseId) {
+  return courses[courseId];
+}
+
+export function getNumCourses(courses) {
+  return Object.keys(courses).length;
 }
