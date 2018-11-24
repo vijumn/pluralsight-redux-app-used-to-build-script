ad41c116bef604ce2ce15954b3bdd91d646ab8c7
Cory House <housecor@gmail.com>
2018-11-24 10:25:58 -0600

    Add error boundary and suspense to root and lazy load courses page

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,32 @@
 // This component handles the App template used on every page.
-import React from "react";
+import React, { Suspense } from "react";
 import PropTypes from "prop-types";
 import { Route } from "react-router-dom";
 import Header from "./common/Header";
 import HomePage from "./home/HomePage";
-import CoursesPage from "./course/CoursesPage";
 import ManageCoursePage from "./course/ManageCoursePage"; //eslint-disable-line import/no-named-as-default
 import AboutPage from "./about/AboutPage";
 import { connect } from "react-redux";
+import ErrorBoundary from "./ErrorBoundary";
+
+// Lazy load the courses page
+const CoursesPage = React.lazy(() => import("./course/CoursesPage"));
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
-        <Header loading={this.props.loading} />
-        <Route exact path="/" component={HomePage} />
-        <Route path="/courses" component={CoursesPage} />
-        <Route path="/course/:id" component={ManageCoursePage} />
-        <Route path="/course" component={ManageCoursePage} exact />
-        <Route path="/about" component={AboutPage} />
-      </div>
+      <ErrorBoundary>
+        <Suspense fallback="Loading...">
+          <div className="container-fluid">
+            <Header loading={this.props.loading} />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/courses" component={CoursesPage} />
+            <Route path="/course/:id" component={ManageCoursePage} />
+            <Route path="/course" component={ManageCoursePage} exact />
+            <Route path="/about" component={AboutPage} />
+          </div>
+        </Suspense>
+      </ErrorBoundary>
     );
   }
 }
