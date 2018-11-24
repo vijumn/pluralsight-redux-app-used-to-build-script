edfa070e7cba5b0f9c68317a869b0d8d1425c7a9
Cory House <housecor@gmail.com>
2018-11-24 10:44:07 -0600

    Declare node env, remove unused loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+process.env.NODE_ENV = "development";
+
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
@@ -13,8 +15,6 @@ module.exports = {
   },
   devServer: {
     // Options: https://webpack.js.org/configuration/dev-server/
-    // quiet: true, // Disable output
-    // open: true, // auto open browser
     stats: "minimal", // output minimal stats to command line
     overlay: true, // overlay errors in browser
     historyApiFallback: true // load deep links
@@ -36,22 +36,6 @@ module.exports = {
             options: { sourcemap: true }
           }
         ]
-      },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
-      {
-        test: /\.(woff|woff2)$/,
-        options: { prefix: "font/", limit: 5000 },
-        loader: "url-loader"
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        options: { limit: 10000, mimetype: "application/octet-stream" },
-        loader: "url-loader"
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        options: { limit: 10000, mimetype: "image/svg+xml" },
-        loader: "url-loader"
       }
     ]
   }
