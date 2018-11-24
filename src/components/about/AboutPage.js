eedf8d385f6cdba5ae80afb99e7f23b38205c82d
Cory House <housecor@gmail.com>
2018-11-24 16:20:12 -0600

    change header

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutPage = () => (
   <div>
-    <h1>About</h1>
+    <h2>About</h2>
     <p>
       This app uses React, Redux, React Router, and many other helpful
       libraries.
