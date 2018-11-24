522c8be26a09584d4b0887038b62965e9d4f2933
Cory House <housecor@gmail.com>
2018-11-23 21:47:53 -0600

    Simplify entry

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "@babel/polyfill";
 import React from "react";
 import { render } from "react-dom";
 import Root from "./components/Root";
-import { AppContainer } from "react-hot-loader";
 import configureStore from "./store/configureStore";
 import { loadCourses } from "./actions/courseActions";
 import { loadAuthors } from "./actions/authorActions";
@@ -16,20 +15,6 @@ store.dispatch(loadCourses());
 store.dispatch(loadAuthors());
 
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+  <Root store={store} history={history} />,
   document.getElementById("app")
 );
-
-if (module.hot) {
-  module.hot.accept("./components/Root", () => {
-    const NewRoot = require("./components/Root").default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById("app")
-    );
-  });
-}
