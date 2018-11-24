2262ac9efc8992550ac05b89a995b60461a5c499
Cory House <housecor@gmail.com>
2018-11-24 11:29:24 -0600

    Streamline

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -2,36 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const CourseList = ({ courses }) => {
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>&nbsp;</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Category</th>
-          <th>Length</th>
+const CourseList = ({ courses }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>&nbsp;</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Category</th>
+        <th>Length</th>
+      </tr>
+    </thead>
+    <tbody>
+      {courses.map(course => (
+        <tr key={course.id}>
+          <td>
+            <a href={course.watchHref}>Watch</a>
+          </td>
+          <td>
+            <Link to={"/course/" + course.id}>{course.title}</Link>
+          </td>
+          <td>{course.authorId}</td>
+          <td>{course.category}</td>
+          <td>{course.length}</td>
         </tr>
-      </thead>
-      <tbody>
-        {courses.map(course => (
-          <tr key={course.id}>
-            <td>
-              <a href={course.watchHref}>Watch</a>
-            </td>
-            <td>
-              <Link to={"/course/" + course.id}>{course.title}</Link>
-            </td>
-            <td>{course.authorId}</td>
-            <td>{course.category}</td>
-            <td>{course.length}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+      ))}
+    </tbody>
+  </table>
+);
 
 CourseList.propTypes = {
   courses: PropTypes.array.isRequired
