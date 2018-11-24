cd2838021d31449ede8269aba6562ac2eef0eadd
Cory House <housecor@gmail.com>
2018-11-23 21:39:16 -0600

    Use cra's server config as inspiration

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,38 +1,66 @@
 /* eslint-disable no-console */
-import express from "express";
 import webpack from "webpack";
-import path from "path";
 import config from "../webpack.config.dev";
-import open from "open";
-import history from "connect-history-api-fallback";
-import webpackDevMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
+import opn from "opn";
+import WebpackDevServer from "webpack-dev-server";
 
 const port = 3000;
-const app = express();
 const compiler = webpack(config);
 
-app.use(history());
-
-app.use(
-  webpackDevMiddleware(compiler, {
-    logLevel: "warn",
-    silent: true,
-    stats: "errors-only",
-    publicPath: config.output.publicPath
-  })
-);
-
-app.use(webpackHotMiddleware(compiler));
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../src/index.html"));
-});
-
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open(`http://localhost:${port}`);
+const serverConfig = {
+  // Enable gzip compression of generated files.
+  compress: true,
+  // Silence WebpackDevServer's own logs since they're generally not useful.
+  // It will still show compile warnings and errors with this setting.
+  clientLogLevel: "none",
+  // By default WebpackDevServer serves physical files from current directory
+  // in addition to all the virtual build products that it serves from memory.
+  // This is confusing because those files won’t automatically be available in
+  // production build folder unless we copy them. However, copying the whole
+  // project directory is dangerous because we may expose sensitive files.
+  // Instead, we establish a convention that only files in `public` directory
+  // get served. Our build script will copy `public` into the `build` folder.
+  // In `index.html`, you can get URL of `public` folder with %PUBLIC_URL%:
+  // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+  // In JavaScript code, you can access it with `process.env.PUBLIC_URL`.
+  // Note that we only recommend to use `public` folder as an escape hatch
+  // for files like `favicon.ico`, `manifest.json`, and libraries that are
+  // for some reason broken when imported through Webpack. If you just want to
+  // use an image, put it in `src` and `import` it from JavaScript instead.
+  contentBase: "./dist",
+  // By default files from `contentBase` will not trigger a page reload.
+  // watchContentBase: true,
+  // Enable hot reloading server. It will provide /sockjs-node/ endpoint
+  // for the WebpackDevServer client so it can learn when the files were
+  // updated. The WebpackDevServer client is included as an entry point
+  // in the Webpack development configuration. Note that only changes
+  // to CSS are currently hot reloaded. JS changes will refresh the browser.
+  hot: true,
+  // It is important to tell WebpackDevServer to use the same "root" path
+  // as we specified in the config. In development, we always serve from /.
+  publicPath: config.output.publicPath,
+  // WebpackDevServer is noisy by default so we emit custom message instead
+  // by listening to the compiler events with `compiler.hooks[...].tap` calls above.
+  quiet: true,
+  host: "http://localhost:3000",
+  proxy: {
+    "/api": {
+      target: "http://localhost:3001"
+    }
+  },
+  historyApiFallback: {
+    // Paths with dots should still use the history fallback.
+    // See https://github.com/facebook/create-react-app/issues/387.
+    disableDotRule: true
   }
+};
+
+const devServer = new WebpackDevServer(compiler, serverConfig);
+// Launch WebpackDevServer.
+devServer.listen(port, "localhost", err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Starting the development server...\n");
+  opn("http://localhost:3000");
 });
