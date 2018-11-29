37664121a31684bb326c3b2bae786680df9d3d01
Cory House <housecor@gmail.com>
2018-11-29 15:37:23 -0600

    Always display header, use spinner, Add ToastContainer

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,11 @@ import PropTypes from "prop-types";
 import { Route } from "react-router-dom";
 import Header from "./common/Header";
 import HomePage from "./home/HomePage";
-import ManageCoursePage from "./course/ManageCoursePage.Hooks"; //eslint-disable-line import/no-named-as-default
+import ManageCoursePage from "./course/ManageCoursePage"; //eslint-disable-line import/no-named-as-default
 import AboutPage from "./about/AboutPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Spinner from "./common/Spinner";
 import { connect } from "react-redux";
 
 // Lazy load the courses page
@@ -13,14 +16,15 @@ const CoursesPage = React.lazy(() => import("./course/CoursesPage"));
 
 const App = ({ loading }) => (
   <div className="container-fluid">
-    <Suspense fallback="Loading...">
-      <Header loading={loading} />
+    <Header loading={loading} />
+    <Suspense fallback={<Spinner />}>
       <Route exact path="/" component={HomePage} />
       <Route path="/courses" component={CoursesPage} />
       <Route path="/course/:id" component={ManageCoursePage} />
       <Route path="/course" component={ManageCoursePage} exact />
       <Route path="/about" component={AboutPage} />
     </Suspense>
+    <ToastContainer autoClose={3000} hideProgressBar />
   </div>
 );
 
