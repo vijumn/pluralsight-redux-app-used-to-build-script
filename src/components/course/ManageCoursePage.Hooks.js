7d82b10977ccf0dd3221df3464a11cdba5cc2ecc
Cory House <housecor@gmail.com>
2018-11-28 18:53:31 -0600

    Sync with class state approach

--- src/components/course/ManageCoursePage.Hooks.js
+++ src/components/course/ManageCoursePage.Hooks.js
@@ -1,25 +1,61 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { saveCourse } from "../../actions/courseActions";
+import { saveCourse, loadCourses } from "../../actions/courseActions";
 import { loadAuthors } from "../../actions/authorActions";
 import CourseForm from "./CourseForm";
 import { authorsFormattedForDropdown } from "../../reducers/authorReducer";
 
 function ManageCoursePage(props) {
-  const { history, loadAuthors, saveCourse, authors } = props;
+  const {
+    history,
+    loadAuthors,
+    saveCourse,
+    authors,
+    loading,
+    courses,
+    loadCourses
+  } = props;
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
   const [saving, setSaving] = useState(false);
 
-  useEffect(() => {
-    if (authors.length === 0) loadAuthors();
-  }, []);
+  useEffect(
+    () => {
+      if (courses.length === 0) {
+        loadCourses();
+      } else {
+        setCourse({ ...props.course });
+      }
+      if (authors.length === 0) loadAuthors();
+    },
+    [props.courses]
+  );
 
-  function handleSaveCourse(event) {
+  function handleChange(event) {
+    setCourse({
+      ...course,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function formIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (course.title.length < 2) {
+      errors.title = "Title must be at least 2 characters.";
+      formIsValid = false;
+    }
+
+    setErrors(errors);
+    return formIsValid;
+  }
+
+  function handleSave(event) {
     event.preventDefault();
 
-    if (!courseFormIsValid()) {
+    if (!formIsValid()) {
       return;
     }
 
@@ -37,39 +73,25 @@ function ManageCoursePage(props) {
       });
   }
 
-  function courseFormIsValid() {
-    let formIsValid = true;
-    let errors = {};
-
-    if (course.title.length < 2) {
-      errors.title = "Title must be at least 2 characters.";
-      formIsValid = false;
-    }
-
-    setErrors(errors);
-    return formIsValid;
-  }
-
   return (
-    <CourseForm
-      course={course}
-      onChange={event => {
-        setCourse({
-          ...course,
-          [event.target.name]: event.target.value
-        });
-      }}
-      onSave={handleSaveCourse}
-      errors={errors}
-      allAuthors={authors}
-      saving={saving}
-    />
+    !loading && (
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSave={handleSave}
+        errors={errors}
+        allAuthors={authors}
+        saving={saving}
+      />
+    )
   );
 }
 
 ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
   saveCourse: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   match: PropTypes.object.isRequired,
@@ -100,6 +122,8 @@ function mapStateToProps(state, ownProps) {
 
   return {
     course: course,
+    courses: state.courses,
+    loading: state.ajaxCallsInProgress > 0,
     authors: authorsFormattedForDropdown(state.authors)
   };
 }
@@ -114,7 +138,8 @@ function mapStateToProps(state, ownProps) {
 
 const mapDispatchToProps = {
   saveCourse,
-  loadAuthors
+  loadAuthors,
+  loadCourses
 };
 
 export default connect(
