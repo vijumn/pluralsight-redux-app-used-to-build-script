4e9c7b1e384afa5e66885ff3e411e1eb06df0e8f
Cory House <housecor@gmail.com>
2018-11-28 21:45:29 -0600

    Remove loading dots

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
-import LoadingDots from "./LoadingDots";
 
-const Header = ({ loading }) => {
+const Header = () => {
   const activeStyle = { color: "#F15B2A" };
   return (
     <nav>
@@ -18,13 +16,8 @@ const Header = ({ loading }) => {
       <NavLink to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
-      {loading && <LoadingDots interval={100} dots={20} />}
     </nav>
   );
-};
-
-Header.propTypes = {
-  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
