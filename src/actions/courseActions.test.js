86b48dd7631bb9ce17ec03a7c72ce74bd4aa3c65
Cory House <housecor@gmail.com>
2018-12-15 08:25:29 -0600

    Use mock data in test

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -1,17 +1,17 @@
 import expect from "expect";
 import * as courseActions from "./courseActions";
 import * as types from "./actionTypes";
-
 import thunk from "redux-thunk";
 import nock from "nock";
 import configureMockStore from "redux-mock-store";
+import { courses } from "../../tools/mockData";
 
 // Test a sync action
 describe("Course Actions", () => {
   describe("createCourseSuccess", () => {
     it("should create a CREATE_COURSE_SUCCESS action", () => {
       //arrange
-      const course = { id: "clean-code", title: "Clean Code" };
+      const course = courses[0];
       const expectedAction = {
         type: types.CREATE_COURSE_SUCCESS,
         course: course
