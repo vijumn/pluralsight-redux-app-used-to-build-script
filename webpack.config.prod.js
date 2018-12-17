f09cdfe600b093ffd835835c079b7e89cd44b775
Cory House <housecor@gmail.com>
2018-12-17 15:16:44 -0600

    Add purgeCssStubs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,12 +3,18 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
+// const glob = require("glob");
+// const PurgecssPlugin = require("purgecss-webpack-plugin");
 
 process.env.NODE_ENV = "production";
 
 const GLOBALS = {
   "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) //This global makes sure React is built in prod mode.
 };
+
+// const PATHS = {
+//   src: path.join(__dirname, "src")
+// };
 
 module.exports = {
   mode: "production",
@@ -26,6 +32,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css"
     }),
+    // new PurgecssPlugin({
+    //   paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
+    // }),
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
       favicon: "src/favicon.ico",
