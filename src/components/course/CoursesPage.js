e914a195541a5544a33b38e79d8ebada26581958
Cory House <housecor@gmail.com>
2018-12-22 14:04:44 -0600

    Eliminate mapDispatchToProps

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import * as courseActions from "../../actions/courseActions";
+import { loadCourses, deleteCourse } from "../../actions/courseActions";
+import { loadAuthors } from "../../actions/authorActions";
 import CourseList from "./CourseList";
-import { coursePropType } from "../propTypes";
+import { coursePropType, authorPropType } from "../propTypes";
 import Spinner from "../common/Spinner";
 import { getCoursesSorted } from "../../reducers/courseReducer";
 import { toast } from "react-toastify";
@@ -16,17 +16,15 @@ class CoursesPage extends React.Component {
   };
 
   componentDidMount() {
-    if (this.props.courses.length === 0) this.props.actions.loadCourses();
+    if (this.props.courses.length === 0) this.props.dispatch(loadCourses());
+    if (this.props.authors.length === 0) this.props.dispatch(loadAuthors());
   }
 
   handleDeleteCourse = course => {
     // Since optimistically deleting, can consider showing success message immediately.
     // There's a tradeoff here though. If the delete ultimately fails, then the user will see a subsequent error message a moment later.
     toast.success("Course deleted");
-    this.props.actions.deleteCourse(course, response => {
-      if (response.error) return toast.error(response.error);
-      // toast.success("Course deleted");
-    });
+    this.props.dispatch(deleteCourse(course, this.handleDeleteResponse));
   };
 
   render() {
@@ -46,6 +44,7 @@ class CoursesPage extends React.Component {
             </button>
 
             <CourseList
+              authors={this.props.authors}
               courses={this.props.courses}
               onDeleteClick={this.handleDeleteCourse}
             />
@@ -57,25 +56,18 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  actions: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
   courses: PropTypes.arrayOf(coursePropType).isRequired,
+  authors: PropTypes.arrayOf(authorPropType).isRequired,
   loading: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
   return {
+    authors: state.authors,
     courses: getCoursesSorted(state.courses),
     loading: state.ajaxCallsInProgress > 0
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseActions, dispatch)
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CoursesPage);
+export default connect(mapStateToProps)(CoursesPage);
