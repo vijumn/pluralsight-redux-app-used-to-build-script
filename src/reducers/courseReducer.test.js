ba62b126b0c31409c12b3aab4017e53ee3da6a74
Cory House <housecor@gmail.com>
2018-12-23 06:07:59 -0600

    Tweak tests

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -6,9 +6,7 @@ describe("Course Reducer", () => {
   it("should add course when passed CREATE_COURSE_SUCCESS", () => {
     // arrange
     const initialState = [{ title: "A" }, { title: "B" }];
-
     const newCourse = { title: "C" };
-
     const action = actions.createCourseSuccess(newCourse);
 
     // act
@@ -24,18 +22,18 @@ describe("Course Reducer", () => {
   it("should update course when passed UPDATE_COURSE_SUCCESS", () => {
     // arrange
     const initialState = [
-      { id: "A", title: "A" },
-      { id: "B", title: "B" },
-      { id: "C", title: "C" }
+      { id: 1, title: "A" },
+      { id: 2, title: "B" },
+      { id: 3, title: "C" }
     ];
 
-    const course = { id: "B", title: "New Title" };
+    const course = { id: 2, title: "New Title" };
     const action = actions.updateCourseSuccess(course);
 
     // act
     const newState = courseReducer(initialState, action);
     const updatedCourse = newState.find(a => a.id == course.id);
-    const untouchedCourse = newState.find(a => a.id == "A");
+    const untouchedCourse = newState.find(a => a.id == 1);
 
     // assert
     expect(updatedCourse.title).toEqual("New Title");
