4cb3f71ce4a2b6dd5e64f7da6dc566c3b9e36085
Cory House <housecor@gmail.com>
2018-12-23 05:14:17 -0600

    Add stub for centralized loading

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
 import ErrorBoundary from "./components/ErrorBoundary";
+// import { loadCourses } from "./actions/courseActions";
+// import { loadAuthors } from "./actions/authorActions";
 import "./styles/styles.css"; //Webpack can import CSS files too!
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const store = configureStore();
+
+// Dispatch actions to load initial state.
+// store.dispatch(loadCourses());
+// store.dispatch(loadAuthors());
 
 render(
   <ErrorBoundary>
