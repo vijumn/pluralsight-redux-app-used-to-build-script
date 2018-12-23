7865de1845710d8c51233399cb3a1cf52cc877b2
Cory House <housecor@gmail.com>
2018-12-23 07:13:48 -0600

    Use triple equal

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -2,7 +2,7 @@ import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
 function actionTypeEndsInSuccess(type) {
-  return type.substring(type.length - 8) == "_SUCCESS";
+  return type.substring(type.length - 8) === "_SUCCESS";
 }
 
 export default function ajaxStatusReducer(
