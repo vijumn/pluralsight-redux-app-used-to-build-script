59c0949822b2170f80fdb8a7aabcb425920efca8
Cory House <housecor@gmail.com>
2018-12-29 07:38:08 -0600

    Start empty

--- src/redux/reducers/initialState.js
+++
@@ -1,5 +0,0 @@
-export default {
-  courses: [],
-  authors: [],
-  ajaxCallsInProgress: 0
-};
--- src/utils/errors.js
+++
@@ -1,7 +0,0 @@
-import { toast } from "react-toastify";
-
-export function toastError(message, error) {
-  toast.error(message + error.message, {
-    autoClose: false
-  });
-}
