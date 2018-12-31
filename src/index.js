dc1d7bde7006a4004fe817b592d4d2f5c2f093a0
Cory House <housecor@gmail.com>
2018-12-30 20:25:03 -0600

    m4 - app structure complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Hi() {
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
