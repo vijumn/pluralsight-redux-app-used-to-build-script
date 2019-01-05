2f510efedc62b61425bbca4c3435b1a91f1140fd
Cory House <housecor@gmail.com>
2019-01-05 17:14:59 -0600

    Rename tool

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 const path = require("path");
 const mockData = require("./mockData");
 
-const data = JSON.stringify(mockData);
+const { courses, authors } = mockData;
+const data = JSON.stringify({ courses, authors });
 const filepath = path.join(__dirname, "db.json");
 
 fs.writeFile(filepath, data, function(err) {
-  err ? console.log(err) : console.log("Component data saved.");
+  err ? console.log(err) : console.log("Mock DB created.");
 });
