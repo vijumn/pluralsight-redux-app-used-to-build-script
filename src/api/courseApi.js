64e9331923d957f6036a94cdc1e366b002afc12c
Cory House <housecor@gmail.com>
2019-01-06 21:35:08 -0600

    Only assign slug on new courses

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -11,8 +11,16 @@ export function saveCourse(course) {
   return fetch(baseUrl + (course.id || ""), {
     method: course.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
     headers: { "content-type": "application/json" },
-    body: JSON.stringify({ ...course, slug: createSlug(course.title) })
+    body: course.id
+      ? JSON.stringify(course)
+      : JSON.stringify({ ...course, slug: createSlug(course.title) })
   })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteCourse(courseId) {
+  return fetch(baseUrl + courseId, { method: "DELETE" })
     .then(handleResponse)
     .catch(handleError);
 }
@@ -25,19 +33,3 @@ function createSlug(value) {
         .replace(/^-|-$/g, "")
         .toLowerCase();
 }
-
-export function deleteCourse(courseId) {
-  return fetch(baseUrl + courseId, { method: "DELETE" })
-    .then(handleResponse)
-    .catch(handleError);
-}
-
-// Async await version
-// export async function deleteCourse(courseId) {
-//   try {
-//     const response = await fetch(baseUrl + courseId, { method: "DELET" });
-//     handleResponse(response);
-//   } catch (error) {
-//     handleError(error);
-//   }
-// }
