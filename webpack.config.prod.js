10c33fe19b7c4d4a26938abe84e1ddf4348b3c96
Cory House <housecor@gmail.com>
2019-01-19 07:21:50 -0600

    Simplify and sync w/ dev config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,19 @@
 const webpack = require("webpack");
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
-// const glob = require("glob");
-// const PurgecssPlugin = require("purgecss-webpack-plugin");
 
 // Required by babel-preset-react-app
 process.env.NODE_ENV = "production";
-
-// const PATHS = {
-//   src: path.join(__dirname, "src")
-// };
 
 module.exports = {
   mode: "production",
   target: "web",
   devtool: "source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
-  // entry: "./src/index", // Can omit, since default.
+  entry: "./src/index", // Can omit, since default.
   output: {
-    path: path.resolve(path.join(__dirname, "dist")), // Note: Physical files are only output by the production build task `npm run build`.
+    path: path.resolve(__dirname, "dist"), // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: "/",
     filename: "bundle.js"
   },
@@ -28,15 +22,12 @@ module.exports = {
     // Typically, this is done either with alternate quotes, such as '"production"', or by using JSON.stringify('production').
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV), // This global makes sure React is built in prod mode.
-      "process.env.API_URL": JSON.stringify("http://localhost:3000") // Would set to prod API URL in real app
+      "process.env.API_URL": JSON.stringify("http://localhost:3001") // Would set to prod API URL in real app
     }),
     new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }), // Display bundle stats
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css"
     }),
-    // new PurgecssPlugin({
-    //   paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
-    // }),
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
       favicon: "src/favicon.ico",
@@ -59,8 +50,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.join(__dirname, "src"),
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        // Processed bottom up, so eslint-loader should be last.
         use: ["babel-loader", "eslint-loader"]
       },
       {
