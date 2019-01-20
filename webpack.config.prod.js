9404adcc2b4017f4d28e50b2aa7cbb200c63ecae
Cory House <housecor@gmail.com>
2019-01-20 08:49:53 -0600

    Add prod webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,79 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
+
+// Required by babel-preset-react-app
+process.env.NODE_ENV = "production";
+
+module.exports = {
+  mode: "production",
+  target: "web",
+  devtool: "source-map", // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  entry: "./src/index", // Can omit, since default.
+  output: {
+    path: path.resolve(__dirname, "build"), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    // Note that because the plugin does a direct text replacement, the value given to it must include actual quotes inside of the string itself.
+    // Typically, this is done either with alternate quotes, such as '"production"', or by using JSON.stringify('production').
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV), // This global makes sure React is built in prod mode.
+      "process.env.API_URL": JSON.stringify("http://localhost:3001") // Would set to prod API URL in real app
+    }),
+    new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }), // Display bundle stats
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    }),
+    // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+      minify: {
+        // see https://github.com/kangax/html-minifier#options-quick-reference
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        // Processed bottom up, so eslint-loader should be last.
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("cssnano"), require("autoprefixer")],
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
