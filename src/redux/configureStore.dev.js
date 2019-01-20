0472758949bb0a3c0cb3c214d1e6beb1b7cccb56
Cory House <housecor@gmail.com>
2019-01-20 09:29:12 -0600

    Fix prod store config

---
+++ src/redux/configureStore.dev.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
+
+  return store;
+}
